6b80c34326ff5c990323c22554ba983dbc50e9e6
pknowledge <jamesblssings@gmail.com>
2024-10-10 10:45:37 +0300

    using add event listener

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the paragraph with the id 'text'
+    const textElement = document.getElementById("text");
+
+    // Change its text content to "This is really cool!"
+    textElement.textContent = "This is really cool!";
+});
